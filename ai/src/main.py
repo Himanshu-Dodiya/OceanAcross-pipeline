c6864410b92f78964a9e82ea0f70530247dc71c8
Himanshu Dodiya <himanshudodiya26@gmail.com>
2026-04-07 01:55:03 +0530

    modified each file in AI,frontend,backend workflows

--- ai/src/main.py
+++ ai/src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def predict():
-    return jsonify({ 'service': 'ai', 'prediction': 'demo', 'status': 'ok' })
+    return jsonify({ 'service': 'ai', 'prediction': 'demo', 'status': 'ok','message': 'AI service is running' })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
