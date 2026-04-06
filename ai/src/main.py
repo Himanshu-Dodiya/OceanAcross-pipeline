63a66e69769178ae535d2cbc1a30fa90e203de9e
Himanshu Dodiya <himanshudodiya26@gmail.com>
2026-04-07 02:04:04 +0530

    modified each file in AI,frontend,backend workflows to see if workflow run as expected

--- ai/src/main.py
+++ ai/src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def predict():
-    return jsonify({ 'service': 'ai', 'prediction': 'demo', 'status': 'ok','message': 'AI service is running' })
+    return jsonify({ 'service': 'ai', 'prediction': 'demo', 'status': 'ok','message': 'AI service is running successfully' })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
